4e318831dbf89f8b4c4cc1d07bf05685fb9aaf7e
George Klitou <5kleig36@solent.ac.uk>
2023-10-27 16:40:33 +0100

    Georgios and Arooj implementing databse on the map

--- src/components/DynamicMap.tsx
+++ src/components/DynamicMap.tsx
@@ -6,32 +6,35 @@ import 'leaflet/dist/leaflet.css';
 // Import the icon image
 import markIcon from '../../public/img/mark.ico';
 
-// Define a custom icon
-const customIcon = new L.Icon({
-  iconUrl: markIcon.src, // Convert StaticImageData to string URL
-  iconSize: [78, 64], // Set your desired icon size
-});
-//Basic Map using leaflet react for now
-export default function DynamicMap (){
-    return (
-<div className="fixed inset-0 flex items-center justify-center">
-  <div className="w-screen h-screen">
-    <MapContainer style={{ width: '100%', height: '100%' }} center={[37.9838, 23.7275]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[37.9838, 23.7275]} icon={customIcon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  </div>
-</div>
 
+//Takes an array of markers to display on the map
+export default function DynamicMap({ mapData }) {
+  //Custom Icon,(work around the default one which is currently broken)
+  const customIcon = new L.Icon({
+    iconUrl: markIcon.src,
+    iconSize: [78, 64],
+  });
 
-
-      );
-};
-
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="w-screen h-screen">
+        {/* Map Rendering */}
+        <MapContainer style={{ width: '100%', height: '100%' }} center={[32.5072, 12.1276]} zoom={10} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+            {/* Rendering a marker for each datapoint in the mapData array */}
+            {/* we assign a unique key to each datapoint using their _id */}
+          {mapData.map((dataPoint) => (
+            <Marker position={[dataPoint.Lat, dataPoint.Lon]} icon={customIcon} key={dataPoint._id}>
+              <Popup>
+                {dataPoint.City} - {dataPoint.Description}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
