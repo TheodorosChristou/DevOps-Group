b188ee8caf52c9a4e82444e2c750638d9feef623
Theo <Theo>
2023-11-28 17:04:45 +0200

    Tested Staging and ready for merge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import { Cloudinary } from '@cloudinary/url-gen';
 import Script from 'next/script';
 import dynamic from 'next/dynamic';
 import { init } from '@sentry/nextjs';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n'; // Adjust the path accordingly
 
 const queryClient = new QueryClient();
 init({
@@ -19,6 +21,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   return (
     <SessionProvider session={session}>
+      <I18nextProvider i18n={i18n}>
         <div className="bg-black min-h-screen pt-[64px] overflow-x-hidden">
           <Script src="https://widget.cloudinary.com/v2.0/global/all.js" />
           <Heading />
@@ -27,6 +30,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
             <Component {...pageProps} />
           </QueryClientProvider>
         </div>
+        </I18nextProvider>
     </SessionProvider>
   );
 }
