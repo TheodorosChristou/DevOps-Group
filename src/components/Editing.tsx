2990e6a8b746cf0890ebbc1b2c0f0971434a6c21
Theo <Theo>
2023-12-03 11:38:15 +0000

    checked version and added some changes

--- src/components/Editing.tsx
+++ src/components/Editing.tsx
@@ -7,13 +7,22 @@ import {useState} from "react";
 import axios from "axios";
 import {useMutation} from "react-query";
 import { redirect } from "next/navigation";
+import { useTranslation } from 'react-i18next';
 
 
 export default function Editing(Locationform){
 
     const values = Locationform.Locationform
 
+    const {t} = useTranslation();
+
     const locationtValue = values
+
+    const [validation, setValidation] = useState(true);
+
+
+    var validate: Boolean
+
 
 
 
@@ -27,9 +36,19 @@ export default function Editing(Locationform){
      asLink ? (window.location.href = url) : window.location.replace(url);
 
       const {isLoading, isSuccess, isError, mutate} = useMutation( async(locationform: FormValues) => {
-            console.log("updating location");
-            await axios.put(`/api/changes/${locationtValue._id}`, locationform);
-            redirect("/")
+        if((locationform.Lat*0 == 0) && (locationform.Lon*0 == 0)){
+          validate = true
+          console.log(validate)
+      }else{
+       setValidation(false)
+       validate = false
+       console.log(validate)
+      }
+      if(validate == true){
+        console.log("updating location");
+        await axios.put(`/api/changes/${locationtValue._id}`, locationform);
+        redirect("/")}
+
           }
         );
 
@@ -41,7 +60,7 @@ export default function Editing(Locationform){
     onSubmit={(locationform) => mutate(locationform) }
     values={locationformValues}
          label="update location"/>
-    </div>
+    <div className="text-white mt-5 flex justify-center">{!validation && (<h1 className="text-white">{t("uploading.sorry")}</h1>)}</div></div> 
         )
 }
 
