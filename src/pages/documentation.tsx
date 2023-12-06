e87ea42a8f84ec5aeb11ba46f399e4a84c0dfad5
Theo <Theo>
2023-12-06 14:58:05 +0000

    checked and verified

--- src/pages/documentation.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-export default function notFound() {
-  const Documentation = dynamic(() => import('../components/Documentation'), { ssr:false} )
-
-  return(<Documentation/>)
-}
