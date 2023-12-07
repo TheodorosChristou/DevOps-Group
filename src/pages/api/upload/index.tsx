9215c09384247172e8aeb929339eeedaf27ecf8e
Theo <Theo>
2023-12-07 13:23:06 +0000

    changed api for swagger

--- src/pages/api/upload/index.tsx
+++ src/pages/api/upload/index.tsx
@@ -8,29 +8,35 @@ import {authOptions} from "../auth/[...nextauth]";
 const handler = async(req,res) =>{
     const {method} = req;
 
-    const session = await unstable_getServerSession(req, res, authOptions);
-    if (!session) {
-      return res.status(404);
-    }
+    const session = await unstable_getServerSession(req, res, authOptions) || null;
+
+    console.log(session)
 
 
-    await dbConnect();
+        await dbConnect();
 
 
-    switch(method){
-        case "POST":
-            try{
-                const map = await Map.create(req.body);
-                res.status(201).json({success: true, data: map})
-            }catch(error) {
+        switch(method){
+            case "POST":
+                try{
+                    if(session == null){
+                        return res.status(401).json({success:false})
+                    }else{
+                        const map = await Map.create(req.body);
+                        return res.status(201).json({success: true, data: map})
+                    }
 
-
-                res.status(400).json({success: false, data: error})
+                }catch(error) {
+    
+    
+                    return res.status(400).json({success: false, data: error})
+                }
+                break;
+    
+                default:
+                    return res.status(400).json({ success: false});
             }
-            break;
 
-            default:
-                res.status(400).json({ success: false});
-    }
+
 };
 export default wrapApiHandlerWithSentry(handler, "/api/upload/index.tsx");
